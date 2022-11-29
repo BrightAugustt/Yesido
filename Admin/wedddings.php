c54d74664c3303bc722ae9da3327725fe9b47e40
BrightAugustt <79414211+BrightAugustt@users.noreply.github.com>
2022-11-29 10:16:23 +0000

    Add-ons

--- Admin/wedddings.php
+++ Admin/wedddings.php
@@ -43,7 +43,7 @@
   <header id="header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
-      <a href="index.html" class="logo d-flex align-items-center">
+      <a href="index.php" class="logo d-flex align-items-center">
         <img src="assets/img/logo.png" alt="">
         <span class="d-none d-lg-block">NiceAdmin</span>
       </a>
@@ -224,7 +224,7 @@
             </li>
 
             <li>
-              <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+              <a class="dropdown-item d-flex align-items-center" href="users-profile.php">
                 <i class="bi bi-person"></i>
                 <span>My Profile</span>
               </a>
@@ -239,7 +239,7 @@
             </li>
 
             <li>
-              <a class="dropdown-item d-flex align-items-center" href="pages-faq.html">
+              <a class="dropdown-item d-flex align-items-center" href="pages-faq.php">
                 <i class="bi bi-question-circle"></i>
                 <span>Need Help?</span>
               </a>
@@ -269,7 +269,7 @@
     <ul class="sidebar-nav" id="sidebar-nav">
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="index.html">
+        <a class="nav-link collapsed" href="index.php">
           <i class="bi bi-grid"></i>
           <span>Dashboard</span>
         </a>
@@ -281,17 +281,17 @@
         </a>
         <ul id="forms-nav" class="nav-content collapse show" data-bs-parent="#sidebar-nav">
           <li>
-            <a href="weddings.html" class="active">
+            <a href="weddings.php" class="active">
               <i class="bi bi-circle"></i><span>Add Wedding</span>
             </a>
           </li>
           <li>
-            <a href="Product-shoots.html" class="active">
+            <a href="Product-shoots.php" class="active">
               <i class="bi bi-circle"></i><span>Add Product Shoots</span>
             </a>
           </li>
           <li>
-            <a href="awards.html" class="active">
+            <a href="awards.php" class="active">
               <i class="bi bi-circle"></i><span>Add Award Ceremonies</span>
             </a>
           </li>
@@ -304,17 +304,17 @@
         </a>
         <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
           <li>
-            <a href="view_weddings.html">
+            <a href="view_weddings.php">
               <i class="bi bi-circle"></i><span>Weddings</span>
             </a>
           </li>
           <li>
-            <a href="view_products.html">
+            <a href="view_products.php">
               <i class="bi bi-circle"></i><span>Product Shoots</span>
             </a>
           </li>
           <li>
-            <a href="view_awards.html">
+            <a href="view_awards.php">
               <i class="bi bi-circle"></i><span>Award Ceremonies</span>
             </a>
           </li>
@@ -327,17 +327,17 @@
         </a>
         <ul id="charts-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
           <li>
-            <a href="charts-chartjs.html">
+            <a href="charts-chartjs.php">
               <i class="bi bi-circle"></i><span>Chart.js</span>
             </a>
           </li>
           <li>
-            <a href="charts-apexcharts.html">
+            <a href="charts-apexcharts.php">
               <i class="bi bi-circle"></i><span>ApexCharts</span>
             </a>
           </li>
           <li>
-            <a href="charts-echarts.html">
+            <a href="charts-echarts.php">
               <i class="bi bi-circle"></i><span>ECharts</span>
             </a>
           </li>
@@ -354,7 +354,7 @@
       <h1>Form Elements</h1>
       <nav>
         <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+          <li class="breadcrumb-item"><a href="index.php">Home</a></li>
           <li class="breadcrumb-item">Forms</li>
           <li class="breadcrumb-item active">Elements</li>
         </ol>
