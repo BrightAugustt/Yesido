100741725711b6788748ace2ad9dd06ba0dcead2
BrightAugustt <79414211+BrightAugustt@users.noreply.github.com>
2022-12-01 15:21:43 +0000

    delete working

--- Admin/view_weddings.php
+++ Admin/view_weddings.php
@@ -28,6 +28,11 @@ include("../controllers/wedding_controller.php");
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
   <!-- Vendor CSS Files -->
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
@@ -431,7 +436,7 @@ include("../controllers/wedding_controller.php");
                                </button>
                              </div>
                              <div class='modal-body'>
-                               <form action='../actions/updatewedding.php' method='POST'>
+                               <form action='../actions/updatewedding.php' method='POST' enctype='multipart/form-data'>
                                   <div class='formgroup'>
                                      <input type='hidden' name='wedding_id' value= '".$wedding[$i]['wedding_id']."'>
                                  </div>
--- actions/updatewedding.php
+++ actions/updatewedding.php
@@ -7,19 +7,37 @@ if(isset($_POST["updatewedding"])){
     $wedding_name = $_POST["wedding_name"];
     $wedding_price = $_POST["wedding_price"];
     $wedding_label = $_POST["wedding_label"];
-    $wedding_img = $_POST["wedding_img"];
     $wedding_key = $_POST["wedding_key"];
+    $img =$_POST['wedding_img'];
 
-    // Shoot Controller
-    $result = updateWedding_ctr($wedding_id,$wedding_name,$wedding_price,$wedding_label,$wedding_img,$wedding_key);
 
-    if($result==true){
-        header('Location:../Admin/view_weddings.php');
+    $output_dir = "../images/wedding/";/* Path for file upload */
+	$RandomNum   = time();
+	$ImageName      = str_replace(' ','-',strtolower($_FILES['wedding_img']['name'][0]));
+	$ImageType      = $_FILES['wedding_img']['type'][0];
+	$ImageExt = substr($ImageName, strrpos($ImageName, '.'));
+	$ImageExt=str_replace('.','',$ImageExt);
+	$ImageName=preg_replace("/\.[^.\s]{3,4}$/", "", $ImageName);
+	$NewImageName = $ImageName.'-'.$RandomNum.'.'.$ImageExt;
+	$ret[$NewImageName]= $output_dir.$NewImageName;
+    
+    if(empty($ImageName)!=TRUE){
+        unlink(("../images/wedding/".$img));
+        move_uploaded_file($_FILES["image"]["tmp_name"][0],$output_dir."/".$NewImageName);
+        if(updateWedding_ctr($wedding_id,$wedding_name,$wedding_price,$wedding_label,$wedding_img,$wedding_key)==TRUE){
+            header('Location:../Admin/view_weddings.php');
+        }else{
+            echo "Failed to delete";
+        }
     }else{
-        echo "Couldn't update changes";
+        if(updateWedding_ctr($wedding_id,$wedding_name,$wedding_price,$wedding_label,$wedding_img,$wedding_key)==TRUE){
+            header('Location:../Admin/view_weddings.php');
+        }else{
+            echo "Failed to delete image";
+        }
     }
 }else{
-    echo "SOmething went wrong";
+    echo "Something went wrong";
 }
 
 
